9ee535c6d0d7158c72831b32a3f4863b9a8e9787
StragelX <stragelx@gmail.com>
2022-02-09 21:34:39 +0200

    f

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  $(function () {
-    $(".marquee").marquee({
-      duplicated: true,
-      direction: "left",
-      duration: 8000,
-    });
-  });
+  // $(function () {
+  //   $(".marquee").marquee({
+  //     duplicated: true,
+  //     direction: "left",
+  //     duration: 8000,
+  //   });
+  // });
 });
