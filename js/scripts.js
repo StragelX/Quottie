4d273c302f633882235f0addf4598d893e38c0b6
StragelX <stragelx@gmail.com>
2022-02-10 09:53:34 +0200

    f

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".feedback_mobile").owlCarousel({
+    items: 1,
+    dots: true,
+    autplay: true,
+    loop: true,
+  });
+});
